b924b6872ec83ec5712b036f791762573029107c
unknown <arturbohanov1908@gmail.com>
2020-03-17 14:14:39 +0200

    Updates

---
+++ precache-manifest.0784392f7163a77e710fec4cb01c64bb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ada708162caa1d51a639af1dd712db0",
+    "url": "/goit-react-hw-02-feedback/index.html"
+  },
+  {
+    "revision": "34a1b85d7147edc2bb29",
+    "url": "/goit-react-hw-02-feedback/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f62bd2dc1f3126889d27",
+    "url": "/goit-react-hw-02-feedback/static/js/2.9e3bc92f.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/goit-react-hw-02-feedback/static/js/2.9e3bc92f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34a1b85d7147edc2bb29",
+    "url": "/goit-react-hw-02-feedback/static/js/main.de556ebb.chunk.js"
+  },
+  {
+    "revision": "8f295445564cac0e313f",
+    "url": "/goit-react-hw-02-feedback/static/js/runtime-main.23cf4f98.js"
+  }
+]);
